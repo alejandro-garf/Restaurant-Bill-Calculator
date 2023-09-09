3ef94bdb08b67e57c29124bfa2dbd861c87e620f
Alejandro Fonseca <123918582+alejandro-garf@users.noreply.github.com>
2023-09-09 04:54:48 +0000

    Prob 01 Submission

--- prob01/main.cc
+++ prob01/main.cc
@@ -1,7 +1,31 @@
 #include <iomanip>
 #include <iostream>
 
+const double kTaxRate = 0.075;
+
 int main() {
-  // TODO: Create a program that computes the restaurant bill.
+  double kMealCost, kTipPercentage, kTaxAmount, kTipAmount, kTotalAmount;
+
+  std::cout << "Please input meal cost: ";
+  std::cin >> kMealCost;
+
+  std::cout << "Please input tip percentage: ";
+  std::cin >> kTipPercentage;
+
+  kTaxAmount = kMealCost * kTaxRate;
+  kTipAmount = kMealCost * (kTipPercentage / 100);
+  kTotalAmount = kMealCost + kTaxAmount + kTipAmount;
+
+  std::cout << std::fixed << std::setprecision(2);
+  std::cout << "\nRestaurant Bill"
+            << "\n";
+  std::cout << "===================="
+            << "\n"
+            << "Subtotal: $" << kMealCost << "\n";
+  std::cout << "Taxes: $" << kTaxAmount << "\n";
+  std::cout << "Tip: $" << kTipAmount << "\n";
+  std::cout << "====================\n";
+  std::cout << "Total: $" << kTotalAmount << "\n";
+
   return 0;
 }
